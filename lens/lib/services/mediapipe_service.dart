7a3ac1ab105957c0733c61c94e0f0d658d188514
Shreyansh <yourname@example.com>
2026-06-25 17:04:18 +0530

    removed old flutter code

--- lens/lib/services/mediapipe_service.dart
+++
@@ -1,7 +0,0 @@
-// lib/services/mediapipe_service.dart
-//
-// ✅ Import ONLY this file anywhere in your app.
-// dart.library.html is available on web with js ^0.6.x
-
-export 'mediapipe_service_mobile.dart'
-    if (dart.library.html) 'mediapipe_service_web.dart';
